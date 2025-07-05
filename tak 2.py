e0edadc368b70fd316a2952bf0117425566d78a4
Parikshit0254 <parikshitsharma901@gmail.com>
2025-07-05 22:05:47 +0530

    Add files via upload

---
+++ tak 2.py
@@ -0,0 +1,6 @@
+
+a=input("enter ur first name= ")
+b=input("enter ur last name= ")
+print("hello,",a,b )
+print("welcome to python program")
+S
---
+++ tak1.py
@@ -0,0 +1,14 @@
+a=input("enter a number= ")
+a=float(a)
+b=input("enter another number= ")
+b=float(b)
+
+
+x=float(a+b)
+print("Addition=" ,x)
+y=float(a-b)
+print("Substraction=" ,y)
+z=float(a*b)
+print("Multiplication=" ,z)
+L=float(a/b)
+print("Division=" ,L)
